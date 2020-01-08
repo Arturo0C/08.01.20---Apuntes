a1cada9e566bc2754e4dd21636a49dcef9e2ef6b
Arturo0C <arturobuendia3@gmail.com>
2020-01-08 20:31:18 +0100

    Initial commit

---
+++ src/pruebas.java
@@ -0,0 +1,11 @@
+import com.matrix.Matrix;
+
+public class pruebas {
+    public static void main(String[] args) {
+
+        double[][] a = {{1,2,3},{1,2,3},{1,2,3}};
+
+        Matrix.printMat(Matrix.add(a,a));
+
+    }
+}
